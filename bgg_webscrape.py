98eb8a16fa37cf5e4ce5b397dfa5d8b0d29e68cf
Adamcules <adam-laing@gurungwe.net>
2022-07-05 20:17:53 +0100

    Open selected category & iterate top 6 games

--- bgg_webscrape.py
+++ bgg_webscrape.py
@@ -6,7 +6,7 @@ category = input("Please enter board game category. Ensure first letter of all w
 
 def open_website(): #open website and accept cookies
     driver = webdriver.Chrome()
-    URL = "https://boardgamegeek.com/"
+    URL = "https://boardgamegeek.com/browse/boardgamecategory"
     driver.get(URL)
     time.sleep(2)
 
@@ -21,23 +21,23 @@ def open_website(): #open website and accept cookies
 driver = open_website()
 driver
 
-def goto_categories(): #goto board game categories section of website
-    time.sleep(2)
-    browse_button = driver.find_element(By.XPATH, '/html/body/gg-app/div/gg-header/header/nav/div/div[1]/div/div[1]/ul/li[1]/button')
-    browse_button.click()
-    time.sleep(2)
-    categories_atag = driver.find_element(By.XPATH, '/html/body/gg-app/div/gg-header/header/nav/div/div[1]/div/div[1]/ul/li[1]/div/div/div/div[1]/span[2]/a')
-    link = categories_atag.get_attribute('href')
-    driver.get(link)
+# def goto_categories(): #goto board game categories section of website
+#     time.sleep(2)
+#     browse_button = driver.find_element(By.XPATH, '/html/body/gg-app/div/gg-header/header/nav/div/div[1]/div/div[1]/ul/li[1]/button')
+#     browse_button.click()
+#     time.sleep(2)
+#     categories_atag = driver.find_element(By.XPATH, '/html/body/gg-app/div/gg-header/header/nav/div/div[1]/div/div[1]/ul/li[1]/div/div/div/div[1]/span[2]/a')
+#     link = categories_atag.get_attribute('href')
+#     driver.get(link)
 
-    return driver
+#     return driver
 
-goto_categories()
+# goto_categories()
 
 def select_category(category): # select inputted category
     category = category
     time.sleep(2)
-    link = driver.find_element(By.PARTIAL_LINK_TEXT, f"{category}")
+    link = driver.find_element(By.LINK_TEXT, f"{category}")
     link.click()
 
     return driver
@@ -45,11 +45,12 @@ def select_category(category): # select inputted category
 select_category(category)
 
 def game_links():
+    time.sleep(2)
     game_container = driver.find_element(By.XPATH, '//*[@class="rec-grid-overview"]') # element containing 6 top games
-    game_list = game_container.find_elements(By.XPATH, './li') # list of games on page
+    game_list = game_container.find_elements(By.XPATH, '//*[@class="rec-grid-overview"]/li') # list of games on page
     link_list = [] # will contain hyperlinks to top 6 games listed
 
-    for game in game_list: #iterate through properties and get hyperlinks
+    for game in game_list: #iterate through games and get hyperlinks
         a_tag = game.find_element(By.TAG_NAME, 'a')
         link = a_tag.get_attribute('href')
         link_list.append(link)
@@ -59,6 +60,18 @@ def game_links():
 game_links()
 
 links = game_links()
+info_list: []
+
+def get_info(): 
+    for hyper in links:
+        driver.get(hyper)
+        
+        time.sleep(2)
+    
+    return driver
+
+get_info()
+
 
 
     
