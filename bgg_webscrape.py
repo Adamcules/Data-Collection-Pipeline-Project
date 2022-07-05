526c75794c3a008b1c4df197e5695488aa948bd6
Adamcules <adam-laing@gurungwe.net>
2022-07-05 22:21:20 +0100

    Start to gather info on games

--- bgg_webscrape.py
+++ bgg_webscrape.py
@@ -47,7 +47,7 @@ select_category(category)
 def game_links():
     time.sleep(2)
     game_container = driver.find_element(By.XPATH, '//*[@class="rec-grid-overview"]') # element containing 6 top games
-    game_list = game_container.find_elements(By.XPATH, '//*[@class="rec-grid-overview"]/li') # list of games on page
+    game_list = game_container.find_elements(By.XPATH, './li') # list of games on page
     link_list = [] # will contain hyperlinks to top 6 games listed
 
     for game in game_list: #iterate through games and get hyperlinks
@@ -64,13 +64,37 @@ info_list: []
 
 def get_info(): 
     for hyper in links:
+        info_dict = {'Name': [], 'Year': [], 'Rating': [], 'Number of Players': [], 'Age': [], 'Wanted By': []}
         driver.get(hyper)
-        
+        time.sleep(2)
+        name = driver.find_element(By.XPATH, '//div[@class="game-header-title-info"]/h1/a').text
+        info_dict['Name'].append(name)
+        year = driver.find_element(By.XPATH, '//div[@class="game-header-title-info"]/h1/span').text
+        info_dict['Year'].append(year)
+        ratings_tab = driver.find_element(By.XPATH, '//*[@id="primary_tabs"]/ul/li[2]')
+        ratings_tab.click()
+        rating = driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/div/ui-view/ui-view/div/ratings-module/div/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/ul/li[1]/div[2]').text
+        info_dict['Rating'].append(rating)
+        player_from = driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/ng-include/div/div[2]/div[2]/div[2]/gameplay-module/div/div/ul/li[1]/div[1]/span/span[1]').text
+        try:
+            player_to = driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/ng-include/div/div[2]/div[2]/div[2]/gameplay-module/div/div/ul/li[1]/div[1]/span/span[2]').text
+        except:
+            player_to = ""
+        num_players = player_from + player_to
+        info_dict['Number of Players'].append(num_players)
+        age = driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/ng-include/div/div[2]/div[2]/div[2]/gameplay-module/div/div/ul/li[3]/div[1]/span').text
+        info_dict['Age'].append(age)
+        stats_tab = driver.find_element(By.XPATH, '//*[@id="primary_tabs"]/ul/li[7]')
+        stats_tab.click()
+        wanted_by = driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/div/ui-view/ui-view/div/div/div[2]/div/div[3]/div[2]/div[2]/ul/li[5]/div[2]/a').text
+        info_dict['Wanted By'].append(wanted_by)
         time.sleep(2)
     
-    return driver
+    return info_dict
 
-get_info()
+print (get_info())
+
+
 
 
 
