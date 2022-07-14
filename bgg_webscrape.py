462a22ec1fdabf29816137f4943e9b82b79fc3b6
Adamcules <adam-laing@gurungwe.net>
2022-07-14 19:29:28 +0100

    Saves game info within specific folder as JSON

--- bgg_webscrape.py
+++ bgg_webscrape.py
@@ -4,10 +4,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
+import uuid
+import os
+import json
 
 class Webscraper:
 
-    def __init__(self): # setup webdriver and info_list which will contain game data from top 6 games of user selected category. ask user to input category.
+    def __init__(self): # setup wedbdriver and info_list which will contain game data from top 6 games of user selected category. ask user to input category.
         self.info_list = []
         self.category = input("Please enter board game category. Ensure first letter of all words is capitalised: ")
         self.driver = webdriver.Chrome()
@@ -18,7 +21,6 @@ class Webscraper:
         self.driver.get(URL)
         time.sleep(1)
         delay = 4
-
         try:
             cookies_button = WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="c-p-bn"]')))
             cookies_button.click()
@@ -30,7 +32,6 @@ class Webscraper:
     def select_category(self, category): # make driver click inputted game category
         time.sleep(1)
         delay = 4
-        
         try:   
             link = WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.LINK_TEXT, f"{category}")))
             link.click()
@@ -44,80 +45,106 @@ class Webscraper:
         game_container = self.driver.find_element(By.XPATH, '//*[@class="rec-grid-overview"]') # element containing top 6 games listed on page
         game_list = game_container.find_elements(By.XPATH, './li') # list of games on page
         link_list = [] # will contain hyperlinks to top 6 games listed
-
         for game in game_list: # iterate through games and get hyperlinks
             a_tag = game.find_element(By.TAG_NAME, 'a')
             link = a_tag.get_attribute('href')
             link_list.append(link)
-
-        return link_list
-
-
-    def get_game_info(self): # get desired info from current game page and store in dictionary
-        info_dict = {'BGG_ID': "", 'Name': "", 'Year': "", 'Rating': "", 'Number of Players': "", 'Age': "", 'Wanted By': "", 'Image': ""}
-        # get bgg game id info:
+        return link_list 
+        
+    def get_id(self): # get bgg game id info
         bgg_id_text = self.driver.find_element(By.XPATH, '//div[@class="game-itemid ng-binding"]').text
-        # extract number (int) from id text:
-        bgg_id = int(''.join(filter(str.isdigit, bgg_id_text)))
-        # add id to dictionary:
-        info_dict['BGG_ID'] = bgg_id
-        # get game name:
+        bgg_id = ''.join(filter(str.isdigit, bgg_id_text))  # extract number (int) from id text
+        return bgg_id
+
+        
+    def get_name(self): # get game name
         name = self.driver.find_element(By.XPATH, '//div[@class="game-header-title-info"]/h1/a').text
-        # add name to dictionary:
-        info_dict['Name'] = name
-        # get year published:    
+        return name
+
+    
+    def get_year(self): # get year published:    
         year = self.driver.find_element(By.XPATH, '//div[@class="game-header-title-info"]/h1/span').text
-        # add year to dictionary: 
-        info_dict['Year'] = year
-        # switch to 'ratings' tab:
-        ratings_tab = self.driver.find_element(By.XPATH, '//*[@id="primary_tabs"]/ul/li[2]')
-        ratings_tab.click()
-        # get game rating:
-        rating = self.driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/div/ui-view/ui-view/div/ratings-module/div/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/ul/li[1]/div[2]').text
-        # add rating to dictionary:
-        info_dict['Rating'] = rating
-        # get 'player from' info:
-        player_from = self.driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/ng-include/div/div[2]/div[2]/div[2]/gameplay-module/div/div/ul/li[1]/div[1]/span/span[1]').text
-        # get 'player to' info (not all games have this element, thus use of 'try' function): 
+        return year
+
+
+    def get_rating(self): # get game rating    
+        ratings_tab = self.driver.find_element(By.XPATH, '//*[@id="primary_tabs"]/ul/li[2]') 
+        ratings_tab.click() # switch to 'ratings' tab 
+        rating = self.driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/div/ui-view/ui-view/div/ratings-module/div/div[2]/div[1]/div[1]/div[2]/div[1]/div[2]/ul/li[1]/div[2]').text # get game rating
+        return rating
+
+        
+    def get_num_players(self):    
+        player_from = self.driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/ng-include/div/div[2]/div[2]/div[2]/gameplay-module/div/div/ul/li[1]/div[1]/span/span[1]').text # get 'player from' info
         try:
-            player_to = self.driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/ng-include/div/div[2]/div[2]/div[2]/gameplay-module/div/div/ul/li[1]/div[1]/span/span[2]').text
+            player_to = self.driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/ng-include/div/div[2]/div[2]/div[2]/gameplay-module/div/div/ul/li[1]/div[1]/span/span[2]').text # get 'player to' info (not all games have this element, thus use of 'try' function)
         except:
             player_to = ""
-        # calculate game number of players. 'From-To' format:
-        num_players = player_from + player_to
-        # add num_players to dictionary:
-        info_dict['Number of Players'] = num_players
-        # get game age rating:
+        num_players = player_from + player_to # calculate game number of players. 'From-To' format
+        return num_players
+
+        
+    def get_age(self): # get game age rating
         age = self.driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/ng-include/div/div[2]/div[2]/div[2]/gameplay-module/div/div/ul/li[3]/div[1]/span').text
-        # add age to dictionary:
-        info_dict['Age'] = age
-        # switch to 'stats' tab:
-        stats_tab = self.driver.find_element(By.XPATH, '//*[@id="primary_tabs"]/ul/li[7]')
-        stats_tab.click()
-        # get 'wanted by' info (number of people expressing wish to get game):
-        wanted_by = self.driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/div/ui-view/ui-view/div/div/div[2]/div/div[3]/div[2]/div[2]/ul/li[5]/div[2]/a').text
-        # add wanted_by to dictionary:
-        info_dict['Wanted By'] = wanted_by
-        # get game image link:
+        return age
+
+        
+    def get_wanted_by(self): # get number of people that have game on wishlist
+        stats_tab = self.driver.find_element(By.XPATH, '//*[@id="primary_tabs"]/ul/li[7]') 
+        stats_tab.click() # switch to 'stats' tab
+        wanted_by = self.driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/div/ui-view/ui-view/div/div/div[2]/div/div[3]/div[2]/div[2]/ul/li[5]/div[2]/a').text # get 'wanted by' info (number of people expressing wish to get game)
+        return wanted_by
+
+        
+    def get_image(self): # get game image link    
         image_xpath = self.driver.find_element(By.XPATH, '//*[@id="mainbody"]/div[2]/div/div[1]/div[2]/ng-include/div/ng-include/div/div[2]/div[1]/ng-include/div/a[1]/img')
         image_src = image_xpath.get_attribute("src")
-        # add image to dictionary:
-        info_dict['Image'] = image_src
+        return image_src
         
-        return info_dict
 
     def iterate_games(self): # open game pages and append game info to info_list. quit driver when complete. 
         time.sleep(2)
         links = self.game_links()
         for hyper in links:
             self.driver.get(hyper)
-            self.info_list.append(self.get_game_info())            
+            info_dict = {'UUID': "", 'BGG_ID': "", 'Name': "", 'Year': "", 'Rating': "", 'Number of Players': "", 'Age': "", 'Wanted By': "", 'Image': ""}
+            info_dict['UUID'] = str(uuid.uuid4())
+            info_dict['BGG_ID'] = self.get_id()
+            info_dict['Name'] = self.get_name()
+            info_dict['Year'] = self.get_year()
+            info_dict['Rating'] = self.get_rating()
+            info_dict['Number of Players'] = self.get_num_players()
+            info_dict['Age'] = self.get_age()
+            info_dict['Wanted By'] = self.get_wanted_by()
+            info_dict['Image'] = self.get_image()
+            self.info_list.append(info_dict)            
             time.sleep(2)
         self.info_list = sorted(self.info_list, key=lambda k: k['Rating'], reverse = True) # sort games by rating value, highest first
-        print (self.info_list)
         self.driver.quit()
+        self.save_dict_records()
+        
 
+    def save_dict_records(self): # save each game dictionary in unique folder within folder 'raw_data'
+        raw_data = '/Users/adam-/OneDrive/Desktop/AI_Core/Data-Collection-Pipeline-Project/raw_data'
+        if not os.path.exists(raw_data):
+            os.mkdir(raw_data)
+        else:
+            print ("raw_data directory already exists")
+        for game in self.info_list:
+            directory = os.path.join(raw_data, game['BGG_ID'])
+            try:
+                os.mkdir(directory)
+            except:
+                print (f"game directory {game['BGG_ID']} already exists")
+            data_file = os.path.join(directory, 'data.json')
+            with open(data_file, 'w') as fp:
+                json.dump(game, fp)
 
+            
 if __name__ == "__main__":
     scrape = Webscraper()
     scrape.open_website()
+
+
+    
+
